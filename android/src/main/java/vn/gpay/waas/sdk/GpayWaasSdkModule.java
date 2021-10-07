e405c43f095908a01f6979e46fd472e03b1263ec
kimbai <bai.nguyenkim@gmail.com>
2021-10-07 19:07:10 +0700

    fix android example

--- android/src/main/java/vn/gpay/waas/sdk/GpayWaasSdkModule.java
+++ android/src/main/java/vn/gpay/waas/sdk/GpayWaasSdkModule.java
@@ -1,5 +1,7 @@
 package vn.gpay.waas.sdk;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Callback;
@@ -7,6 +9,8 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.module.annotations.ReactModule;
 import com.openwallet.ui.widget.GpayWAASSDK;
 
@@ -29,20 +33,30 @@ public class GpayWaasSdkModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void initSDK(String appId, String env, Callback callback) {
     GpayWAASSDK.Enviroment sdkEnv;
-    if (env.toLowerCase() == "development") {
+    if (env.toLowerCase().equals("development")) {
       sdkEnv = GpayWAASSDK.Enviroment.DEVELOPMENT;
-    } else if (env.toLowerCase() == "sandbox") {
+    } else if (env.toLowerCase().equals("sandbox")) {
       sdkEnv = GpayWAASSDK.Enviroment.SANDBOX;
     } else {
       sdkEnv = GpayWAASSDK.Enviroment.PRODUCT;
     }
-    GpayWAASSDK.initializeWith(this.getReactApplicationContext(), appId, sdkEnv, result -> callback.invoke(result));
+    GpayWAASSDK.initializeWith(this.getReactApplicationContext(), appId, sdkEnv, new GpayWAASSDK.onComplete() {
+      @Override
+      public void onComplete(HashMap<String, String> result) {
+        if (result != null) {
+          WritableMap map = new WritableNativeMap();
+          map.putString("code", result.get("code"));
+          map.putString("message", result.get("message"));
+          callback.invoke(map);
+        }
+      }
+    });
   }
 
   @ReactMethod
   public void setupTheme(String theme) {
     GpayWAASSDK.Theme sdkTheme;
-    if (theme.toLowerCase() == "dark") {
+    if (theme.toLowerCase().equals("dark")) {
       sdkTheme = GpayWAASSDK.Theme.DARK;
     } else {
       sdkTheme = GpayWAASSDK.Theme.LIGHT;
@@ -53,7 +67,7 @@ public class GpayWaasSdkModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void setupLanguage(String language) {
     GpayWAASSDK.Language sdkLanguage;
-    if (language.toLowerCase() == "vi") {
+    if (language.toLowerCase().equals("vi")) {
       sdkLanguage = GpayWAASSDK.Language.VI;
     } else {
       sdkLanguage = GpayWAASSDK.Language.EN;
@@ -63,7 +77,17 @@ public class GpayWaasSdkModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void homeOpen(String userId, String phoneNumber, Callback callback) {
-    GpayWAASSDK.getInstance().homeOpen(this.getCurrentActivity(), userId, phoneNumber, result -> callback.invoke(result));
+    GpayWAASSDK.getInstance().homeOpen(this.getCurrentActivity(), userId, phoneNumber, new GpayWAASSDK.onComplete() {
+      @Override
+      public void onComplete(HashMap<String, String> result) {
+        if (result != null) {
+          WritableMap map = new WritableNativeMap();
+          map.putString("code", result.get("code"));
+          map.putString("message", result.get("message"));
+          callback.invoke(map);
+        }
+      }
+    });
   }
 
   @ReactMethod
@@ -71,7 +95,12 @@ public class GpayWaasSdkModule extends ReactContextBaseJavaModule {
     GpayWAASSDK.getInstance().getUserStatus(this.getCurrentActivity(), new GpayWAASSDK.onComplete() {
       @Override
       public void onComplete(HashMap<String, String> result) {
-        callback.invoke(result);
+        if (result != null) {
+          WritableMap map = new WritableNativeMap();
+          map.putString("code", result.get("code"));
+          map.putString("message", result.get("message"));
+          callback.invoke(map);
+        }
       }
     });
   }
@@ -81,7 +110,12 @@ public class GpayWaasSdkModule extends ReactContextBaseJavaModule {
     GpayWAASSDK.getInstance().getUserBalance(this.getCurrentActivity(), new GpayWAASSDK.onComplete() {
       @Override
       public void onComplete(HashMap<String, String> result) {
-        callback.invoke(result);
+        if (result != null) {
+          WritableMap map = new WritableNativeMap();
+          map.putString("code", result.get("code"));
+          map.putString("message", result.get("message"));
+          callback.invoke(map);
+        }
       }
     });
   }
